a7fb0ae1e559fdc6916bfc5b3b485e935749152c
george-shammar <george.gbenle@outlook.com>
2024-05-05 23:31:09 +0100

    Create Leaderboard

--- contracts/Leaderboard.sol
+++ contracts/Leaderboard.sol
@@ -3,66 +3,36 @@
 pragma solidity ^0.8.0;
 
 contract Leaderboard {
-  mapping (address => Player) public players;
-  address[] public rankings;
-  address[] playerAddresses;
+    struct Player {
+        uint256 score;
+        bool exists;
+    }
 
-  struct Player {
-    uint256 score;
-  }
+    mapping(address => Player) public players;
+    address[] public rankings;
 
-  function addPlayer(address _player, uint256 _score) public {
-    players[_player].score = _score;
-    updateRankings();
-  }
+    event PlayerAdded(address player, uint256 score);
+    event RankingsUpdated(address[] newRankings);
 
-  function removePlayer(address _player) public {
-    delete players[_player];
-    updateRankings();
-  }
+    function addPlayer(address _player, uint256 _score) public {
+        require(!players[_player].exists, "Player already exists");
 
-  function updateScore(address _player, uint256 _score) public {
-    players[_player].score = _score;
-    updateRankings();
-  }
+        players[_player] = Player(_score, true);
+        rankings.push(_player);
 
-  function bubbleSortDescending(uint256[] memory arr) private pure {
-    uint256 n = arr.length;
-    for (uint256 i = 0; i < n - 1; i++) {
-        for (uint256 j = 0; j < n - i - 1; j++) {
-            if (arr[j] < arr[j + 1]) {
-                // Swap arr[j] and arr[j+1]
-                uint256 temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+        emit PlayerAdded(_player, _score);
+        updateRankings();
+    }
+
+    function updateRankings() private {
+        for (uint256 i = 0; i < rankings.length; i++) {
+            for (uint256 j = i + 1; j < rankings.length; j++) {
+                if (players[rankings[i]].score < players[rankings[j]].score) {
+                    (rankings[i], rankings[j]) = (rankings[j], rankings[i]);
+                }
             }
         }
-    }
-  }
 
-  function updateRankings() private {
-    uint256[] memory scores = new uint256[](playerAddresses.length);
-    uint256 i = 0;
-    for (uint256 j = 0; j < playerAddresses.length; j++) {
-        address player = playerAddresses[j];
-        scores[i] = players[player].score;
-        i++;
+        emit RankingsUpdated(rankings);
     }
-    bubbleSortDescending(scores);
-    i = 0;
-    for (uint256 k = 0; k < scores.length; k++) {
-        uint256 score = scores[k];
-        for (uint256 l = 0; l < playerAddresses.length; l++) {
-            address player = playerAddresses[l];
-            if (players[player].score == score) {
-                rankings[i] = player;
-                i++;
-            }
-        }
-    }
-  }
-
-  function greaterThan(uint256 a, uint256 b) private pure returns (bool) {
-    return a > b;
-  }
 }
