574b9028723d70394e247333a3f229aafb68b529
george-shammar <george.gbenle@outlook.com>
2024-05-05 23:45:56 +0100

    Create Leaderboard

--- contracts/Sorcerer.sol
+++ contracts/Sorcerer.sol
@@ -7,65 +7,54 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/security/Pausable.sol";
 
-contract Sorcerer is ERC721URIStorage, Pausable, Ownable {
-  constructor(
-    string memory _name,
-    string memory _symbol
-  )ERC721(_name, _symbol){
+contract Sorcerer is ERC721 {
+    constructor(
+        string memory _name,
+        string memory _symbol
+    ) ERC721(_name, _symbol) {}
 
-  }
+    uint256 COUNTER;
 
-  uint256 COUNTER;
+    uint256 private _price = 1200000000000000;
 
-  uint256 private _price = 1200000000000000;
-
-  struct Sorcer {
-    string name;
-    uint256 id;
-    uint256 level;
-  }
-
-  Sorcer[] public sorcerers;
-
-  event NewSorcerer(address indexed owner, uint256 id, string name, uint256 level);
-
-  // Creation
-  function _createSorcerer(string memory _name, string memory tokenURI) internal {
-   Sorcer memory newSorcerer = Sorcer(_name, COUNTER, 1);
-    sorcerers.push(newSorcerer);
-    _safeMint(msg.sender, COUNTER);
-    _setTokenURI(COUNTER, tokenURI);
-    emit NewSorcerer(msg.sender, COUNTER, _name, 1);
-    COUNTER++;
-  }
-
-  function createSorcerer(string memory _name, string memory tokenURI) public
-  whenNotPaused {
-    _createSorcerer(_name, tokenURI);
-  }
-
-   // Getters
-  function getSorcerers() public view returns (Sorcer[] memory) {
-    return sorcerers;
-  }
-
-  function getOwnerSorcerers(address _owner) public view returns (Sorcer[] memory) {
-    Sorcer[] memory result = new Sorcer[](balanceOf(_owner));
-    uint256 counter = 0;
-    for (uint256 i = 0; i < sorcerers.length; i++) {
-      if (ownerOf(i) == _owner) {
-        result[counter] = sorcerers[i];
-        counter++;
-      }
-    }
-    return result;
-  }
-
-  function pause() public onlyOwner {
-        _pause();
+    struct Sorcer {
+        string name;
+        uint256 id;
+        uint256 level;
     }
 
-  function unpause() public onlyOwner {
-        _unpause();
-  }
+    Sorcer[] public sorcerers;
+
+    event NewSorcerer(address indexed owner, uint256 id, string name, uint256 level);
+
+    // Creation
+    function _createSorcerer(string memory _name, address _owner) internal {
+        Sorcer memory newSorcerer = Sorcer(_name, COUNTER, 1);
+        sorcerers.push(newSorcerer);
+        _safeMint(_owner, COUNTER);
+        // _setTokenURI(COUNTER, tokenURI);
+        emit NewSorcerer(_owner, COUNTER, _name, 1);
+        COUNTER++;
+    }
+
+    function createSorcerer(string memory _name) public {
+        _createSorcerer(_name, msg.sender);
+    }
+
+    // Getters
+    function getSorcerers() public view returns (Sorcer[] memory) {
+        return sorcerers;
+    }
+
+    function getOwnerSorcerers(address _owner) public view returns (Sorcer[] memory) {
+        Sorcer[] memory result = new Sorcer[](balanceOf(_owner));
+        uint256 counter = 0;
+        for (uint256 i = 0; i < sorcerers.length; i++) {
+            if (ownerOf(sorcerers[i].id) == _owner) {
+                result[counter] = sorcerers[i];
+                counter++;
+            }
+        }
+        return result;
+    }
 }
