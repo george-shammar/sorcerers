17ccda9a25eec5bc021df8fd5311fff3c6bf5ff3
george-shammar <george.gbenle@outlook.com>
2024-05-12 09:18:33 +0100

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,6 @@ echo "Building app"
 npm run build:site
 
 echo "Deploying files to server"
-rsync -avP dist/ root@45.33.31.71:/var/www/45.33.31.71
+# rsync -avP dist/ root@45.33.31.71:/var/www/45.33.31.71
+rsync -avP dist/ root@176.58.127.136:/var/www/sorcerer.livewirre.com
 echo "Deployment complete"
